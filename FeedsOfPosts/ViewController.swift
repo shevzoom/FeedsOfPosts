7c268409dff158eca55d01bb2ea00bc3a423197d
Gleb <64494962+shevzoom@users.noreply.github.com>
2021-09-03 14:34:15 +0300

    Initial Commit

---
+++ FeedsOfPosts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeedsOfPosts
+//
+//  Created by Глеб Шевченко on 03.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
